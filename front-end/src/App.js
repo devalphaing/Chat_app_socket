0711d9cda9b67a4786405dee1020b522ca108a16
Devang Sharma <devangsharma7861@gmail.com>
2024-07-10 00:33:36 +0530

    React routing added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,34 @@
 import './App.css';
-import ChatPage from './components/ChatPage';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+  Route,
+  createRoutesFromElements
+} from "react-router-dom";
+import ChatPage from './pages/ChatPage';
+import Login from './pages/Login';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const ProtectedRoute = ({ element }) => {
+    return isLoggedIn ? element : <Navigate to="/login" />;
+  };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="*" element={isLoggedIn ? <Navigate to="/" replace /> : <Login />} />
+        <Route path="/" element={<ProtectedRoute element={<ChatPage />} />} />
+        <Route path="/login" element={isLoggedIn ? <Navigate to="/" replace /> : <Login />} />
+      </>
+    )
+  );
+
   return (
-    <div className="App">
-      <ChatPage />
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
